7d57b4c349e97a8b495934d9fc6bb38987f09736
dryg <xboxseriaskell@gmail.com>
2023-02-17 22:55:37 +0500

    урок по видео. тест

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("привет мир!");
+
+        float sugar = 3;
+        float onePortion = sugar/4;
+        System.out.println("Одна порция сахара"+ onePortion+"кг!" );
+        byte a = 1;
+        short b = 1;
+        int c = 1;
+        int d = a + b + c;
+        System.out.println(d);
+        float g = 1f;
+        System.out.println(g);
+
     }
 }
