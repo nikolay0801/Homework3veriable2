b37f5aed138f09fb13aff4f6609c0f5a50219f79
dryg <xboxseriaskell@gmail.com>
2023-02-21 20:31:39 +0500

    домашнее задание 6

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,25 @@ public class Main {
     public static void main(String[] args)
     {
 
-        float sugar = 3;
-        float onePortion = sugar/4;
-        System.out.println("Одна порция сахара"+ onePortion+"кг!" );
-        byte a = 1;
-        short b = 1;
-        int c = 1;
-        int d = a + b + c;
-        System.out.println(d);
-        float g = 1f;
-        System.out.println(g);
+        long bananes = 80;
+        long weigtBananes = bananes * 5;
+        System.out.println("Общий вес бананов " + weigtBananes + "граммах!");
+        long milk = 105;
+        long weightMilk = milk * 2;
+        System.out.println("Общий вес молока " + weightMilk + "граммах!");
+        long iceCreame = 100;
+        long weightIceCreame = iceCreame * 2;
+        System.out.println("Общий вес мороженного " +  weightIceCreame+ "граммах!");
+        long egg  = 70;
+        long weightEgg = egg * 4;
+        System.out.println("Общий вес яиц " + weightEgg+ "граммах!");
+        float totalWeightOfProducts = weigtBananes + weightMilk + weightIceCreame + weightEgg;
+        System.out.println("Общий вес продуктов" + totalWeightOfProducts / 1000 + "кг!");
 
-        byte ludmilaPavlovna = 23;
-        byte annaSergeevna = 27;
-        byte ekaterinaAndreevna = 30;
-        int studentAll = ludmilaPavlovna + annaSergeevna + ekaterinaAndreevna;
-        System.out.println("Всего учеников"+ studentAll +"в 3х классах");
-         short peperAll = 480;
-        System.out.println("На каждого ученика"+ ( peperAll/studentAll ) +"Листов бумаги");
+
+
+
+
 
         {
     }
